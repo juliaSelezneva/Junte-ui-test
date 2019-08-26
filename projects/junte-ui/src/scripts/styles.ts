6946e6974119032490b4b70dd534bf6ff0f8385e
juliaselezneva <juli.89@mail.ru>
2019-08-26 11:07:09 +0300

    fixes

--- projects/junte-ui/src/scripts/styles.ts
+++ projects/junte-ui/src/scripts/styles.ts
@@ -1,23 +1,18 @@
 import { writeFileSync } from 'fs';
 import * as gulp from 'gulp';
-import * as debug from 'gulp-debug';
-import * as watch from 'gulp-watch';
 import { Gulpclass, SequenceTask, Task } from 'gulpclass';
 import * as map from 'map-stream';
-import * as path from 'path';
 import 'reflect-metadata';
 
 const argument = require('minimist')(process.argv.slice(2));
-const styleFiles = '../lib/components/**/*.scss';
-const commonStyleFiles = '!../lib/components/**/*.component.scss';
+const styleFiles = '../lib/components/**/{common,*.encapsulated}.scss';
 
 @Gulpclass()
 export class Gulpfile {
 
   @Task()
   styles() {
-    return gulp.src([styleFiles, commonStyleFiles], {'base': 'src/lib/components'})
-      .pipe(debug())
+    return gulp.src([styleFiles], {'base': 'src/lib/components'})
       .pipe(map((file, cb) => {
         const contents = file.contents.toString();
         try {
@@ -42,6 +37,6 @@ export class Gulpfile {
 
   @Task()
   watch() {
-    return watch([styleFiles, commonStyleFiles], {ignoreInitial: false}, gulp.series('styles'));
+    return gulp.watch([styleFiles], {ignoreInitial: false}, gulp.series('styles'));
   }
 }
