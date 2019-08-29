120430a675fc19b81bd9c285bd9abe6b13505c44
Vladimir Smirnov <ex700380@gmail.com>
2019-08-29 13:22:00 +0300

    #431 refactor month gantt

--- src/components/documentation/gantt/gantt-test.component.ts
+++ src/components/documentation/gantt/gantt-test.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { GanttRequestStatuses } from 'junte-ui';
+import { UI } from 'projects/junte-ui/src/lib/enum/ui';
 import { REQUESTS } from './requests';
 
 @Component({
@@ -10,6 +11,7 @@ import { REQUESTS } from './requests';
 })
 export class GanttTestComponent implements OnInit {
 
+  ui = UI;
   requests = [];
   statuses = GanttRequestStatuses;
   loading = true;
@@ -27,5 +29,24 @@ export class GanttTestComponent implements OnInit {
       this.loading = false;
     }, 3000);
     this.gantt.valueChanges.subscribe(date => console.log('Date changed: ', date));
+  }
+
+  add() {
+    this.requests.push({
+      'id': 111,
+      'createdAt': '2019-08-25T01:11:40+0300',
+      'createdBy': {
+        'id': 111,
+        'login': 'Alexeev Alexey Alexeevich'
+      },
+      'from': '2019-08-25T07:19:56+0300',
+      'to': '2020-01-01T10:05:24+0300',
+      'selfExpense': true,
+      'status': 'accepting'
+    });
+  }
+
+  remove() {
+    this.requests.splice(Math.floor(Math.random() * this.requests.length), 1);
   }
 }
