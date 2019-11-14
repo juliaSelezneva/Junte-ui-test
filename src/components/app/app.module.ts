c97d3cd6a08964b7ba23f2db30b00f456bd25a92
juliaselezneva <juli.89@mail.ru>
2019-11-14 17:16:48 +0300

    fix import

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -1,13 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { JunteUiModule } from 'junte-ui';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
